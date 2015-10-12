a0fea38607e6c75a17158433a70bc4d69cd090ff
Benoit Tremblay <trembl.ben@gmail.com>
2015-10-12 17:55:59 -0400

    Release 0.1.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'webpack:reload',
-    version: '0.1.0',
+    version: '0.1.1',
     summary: 'disable the browser reload for webpack (DO NOT INCLUDE THIS PACKAGE BY YOURSELF)',
     git: 'https://github.com/thereactivestack/meteor-webpack.git',
     documentation: 'README.md',
