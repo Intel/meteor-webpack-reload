63e6ee2f5fcc1f3f4ad4f508995d73966892b605
Benoit Tremblay <trembl.ben@gmail.com>
2015-10-12 22:03:26 -0400

    Fix cordova

--- reload.js
+++ reload.js
@@ -7,13 +7,20 @@ Meteor.startup(function() {
   // Use the script from the webpack dev server
   var target = Meteor.isCordova ? 'cordova' : 'web';
 
+  var devServerOrigin = window.location.origin.split(/:[0-9]+$/)[0] + ':3500';
+
   const scriptEl = document.createElement('script');
   scriptEl.type = 'text/javascript';
-  scriptEl.src = `http://localhost:3500/assets/common.${target}.js`;
+  scriptEl.src = `${devServerOrigin}/assets/common.${target}.js`;
   document.head.appendChild(scriptEl);
 
   const scriptEl2 = document.createElement('script');
   scriptEl2.type = 'text/javascript';
-  scriptEl2.src = `http://localhost:3500/assets/${target}.js`;
+  scriptEl2.src = `${devServerOrigin}/assets/${target}.js`;
   document.head.appendChild(scriptEl2);
 });
+
+if (Meteor.isCordova) {
+  // Allow access to the dev server
+  App.accessRule('*');
+}
