852c9f72600af2e76d714fd4a4d812215c23744e
Benoit Tremblay <trembl.ben@gmail.com>
2015-10-14 10:27:44 -0400

    Fix ES6 + release 0.1.4

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'webpack:reload',
-    version: '0.1.3',
+    version: '0.1.4',
     summary: 'disable the browser reload for webpack (DO NOT INCLUDE THIS PACKAGE BY YOURSELF)',
     git: 'https://github.com/thereactivestack/meteor-webpack.git',
     documentation: 'README.md',
@@ -11,6 +11,6 @@ Package.onUse(function(api) {
     api.versionsFrom('1.2');
 
     api.imply(['reload'], 'client');
-    api.use(['reload'], 'client');
+    api.use(['reload', 'ecmascript@0.1.5'], 'client');
     api.addFiles('reload.js', 'client');
 });
